a0129f649ec2872fd0072d304cf30769f69e7b10
ZapVuVan <shionsv267@gmail.com>
2025-12-29 13:36:48 +0700

    HandleInteraction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,44 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
     private void Update()
     {
+        HandleMovement();  
+        HandleInteractions();
+
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if(moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance,countersLayerMask))
+        {
+           if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
+                // Has ClearCounter
+                clearCounter.Interact();
+           }
+        }
        
+    }
+    private void HandleMovement()
+    {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
@@ -28,7 +61,8 @@ public class Player : MonoBehaviour
             {
                 //can move only on X
                 moveDir = moveDirX;
-            }else
+            }
+            else
             {
                 // cannot move only on X
 
@@ -55,11 +89,5 @@ public class Player : MonoBehaviour
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        Debug.Log(Time.deltaTime);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }
